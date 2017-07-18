15a70a0fb8fbba80da6e4a3bec779e4d0d5a9e7a
qixiafei <qixiafei@le.com>
2017-07-18 15:32:24 +0800

    完成资源文件生成方法

---
+++ src/main/java/com/xiafei/tools/generatesource/template/DaoTemplate.java
@@ -0,0 +1,15 @@
+package com.xiafei.tools.generatesource.template;
+
+/**
+ * <P>Description:  </P>
+ * <P>CALLED BY:   齐霞飞 </P>
+ * <P>UPDATE BY:   齐霞飞 </P>
+ * <P>CREATE DATE: 2017/7/18</P>
+ * <P>UPDATE DATE: 2017/7/18</P>
+ *
+ * @author qixiafei
+ * @version 0.0.3-SNAPSHOT
+ * @since java 1.7.0
+ */
+public class DaoTemplate {
+}
