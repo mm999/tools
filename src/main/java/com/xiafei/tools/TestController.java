64ebce8436ee2257e468a83f48303f11d87d605c
qixiafei <qixiafei@le.com>
2017-12-02 00:43:12 +0800

    增加solr客户端配置

---
+++ src/main/java/com/xiafei/tools/TestController.java
@@ -0,0 +1,48 @@
+package com.xiafei.tools;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrInputDocument;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+/**
+ * <P>Description: 测试用. </P>
+ * <P>CALLED BY:   齐霞飞 </P>
+ * <P>UPDATE BY:   齐霞飞 </P>
+ * <P>CREATE DATE: 2017/12/2</P>
+ * <P>UPDATE DATE: 2017/12/2</P>
+ *
+ * @author 齐霞飞
+ * @version 1.0
+ * @since java 1.7.0
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Resource
+    private SolrClient solrClient;
+
+    @RequestMapping("/solr/add")
+    public void testSolrAdd() throws IOException, SolrServerException {
+        final SolrInputDocument solrInputFields = new SolrInputDocument();
+        solrInputFields.setField("id", "1");
+        solrInputFields.setField("name", "齐霞飞");
+        solrInputFields.setField("password", "qxf");
+        solrClient.add(solrInputFields);
+        solrClient.commit();
+    }
+
+
+    @RequestMapping("/solr/query")
+    public String testSolrQuery() throws IOException, SolrServerException {
+        final SolrQuery query = new SolrQuery();
+        query.set("q", "id:1");
+        return solrClient.query(query).getResults().toString();
+    }
+}
